7f2797455580493d03805af99c6b8f922c69f769
Buket Sahin <buki.mtuna@gmail.com>
2025-07-22 09:11:18 +0300

    hw12-2

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
+
 export default defineConfig(({ command }) => {
   return {
-    base: '/goit-js-hw-12/',
+    base: 'goit-js-hw-12',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
